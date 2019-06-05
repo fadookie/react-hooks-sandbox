177e9ad6bf3268ad22629fdc5037da9e4060aca3
Eliot Lash <fadookie@gmail.com>
2019-06-05 09:38:04 -0700

    Add component index`

--- src/App.js
+++ src/App.js
@@ -7,27 +7,44 @@ const setClampedCount = (newCount, setCount) => {
   setCount(_.clamp(newCount, 1, 100));
 };
 
-const displayFizzBuzz = count => {
+const displayFizzBuzz = (count) => {
   if (count % 15 === 0) {
     return 'FizzBuzz';
   } else if (count % 3 === 0) {
     return 'Fizz';
   } else if (count % 5 === 0) {
     return 'Buzz';
-  } else {
-    return count;
   }
-}
+  return count;
+};
+
+const FizzBuzz = () => {
+  const [count, setCount] = useState(1);
+  return (
+    <>
+      <p>{displayFizzBuzz(count)}</p>
+      <button onClick={() => setClampedCount(count + 1, setCount)}>+</button>
+      <button onClick={() => setClampedCount(count - 1, setCount)}>-</button>
+    </>
+  );
+};
 
 const App = () => {
-  const [count, setCount] = useState(1);
+  const [activeComponent, setActiveComponent] = useState('FizzBuzz');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{displayFizzBuzz(count)}</p>
-        <button onClick={() => setClampedCount(count + 1, setCount)}>+</button>
-        <button onClick={() => setClampedCount(count - 1, setCount)}>-</button>
+        <button onClick={() => setActiveComponent('FizzBuzz')}>FizzBuzz</button>
+        <button onClick={() => setActiveComponent('Other')}>Other</button>
+        {(() => {
+          switch (activeComponent) {
+            case 'FizzBuzz':
+              return <FizzBuzz />;
+            default:
+              return `Unknown component ${activeComponent}`;
+          }
+        })()}
       </header>
     </div>
   );
